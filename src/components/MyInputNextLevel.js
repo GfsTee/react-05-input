c0510344a4212448c66245e86df07d45e53b8c19
GFSTee <georg.treitz@gmail.com>
2020-11-30 10:35:34 +0100

    input

---
+++ src/components/MyInputNextLevel.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class MyInput extends Component {
+    state = {
+        value1: "",
+        value2: "",
+        value3: "",
+        collection: [
+
+        ]
+    }
+    handleChange = (event) => {
+        console.log(event.target.name);
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    handleAdd = () => {
+        let newObj = { firstName: this.state.value1, lastName: this.state.value2, age: this.state.value3 }
+        let collection = this.state.collection
+        collection.push(newObj)
+        this.setState({ collection }); // Hier wird der state collection mit der Variablen collection geupdatet
+        this.setState({ value1: "", value2: "", value3: "" });
+
+    }
+    render() {
+        return (
+            <div>
+                <input type="text" name="value1" value={this.state.value1} onChange={this.handleChange} /> <br />
+                <input type="text" name="value2" value={this.state.value2} onChange={this.handleChange} /> <br />
+                <input type="text" name="value3" value={this.state.value3} onChange={this.handleChange} /> <br />
+                <button onClick={this.handleAdd}>Add</button>
+                {this.state.collection.map((ele, i) => <div key={i}>The User is {ele.firstName} {ele.lastName} and is  {ele.age}</div>)}
+            </div>
+        );
+    }
+}
+
+export default MyInput;
